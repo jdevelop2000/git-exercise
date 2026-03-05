319dc3a6b1001570789bac67a83498236f4193d5
Junsoo Kim <ds5gre@gmail.com>
2026-03-05 17:35:30 +0900

    update in third_test.py

--- third_test.py
+++ third_test.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
 print ("this is created by sub developer")
-ptint ("now i am adding code more")
+print ("now i am adding code more")
+print ("I have fixed one bug in line 4")
 
